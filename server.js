02fe0cf67afaf90ac5c7f18ce4bffa482dd88801
clshu <ccss0630@yahoo.com>
2017-01-20 03:04:44 -0600

    Set up server to serve favicon.ico

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var methodOverride = require('method-override');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 //var events = require('./api/routes/events.js');
 
 
 var PORT = process.env.PORT || 3000;
 var app = express();
 
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
 	extended: false 
