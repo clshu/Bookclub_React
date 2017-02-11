10a9f1b2dfa3fb25574bc1c5e7dfe8b2caf30eb4
bhagya <bhagya@outlook.com>
2017-02-10 20:38:27 -0600

    added activation for event list

--- app/components/Events.js
+++ app/components/Events.js
@@ -9,6 +9,7 @@ class Events extends Component{
   componentDidMount() {
 
     if (this.props.events.length == 0) {
+      console.log("Events: Reloading")
       this.props.getEvents();
     }
   }
@@ -16,18 +17,35 @@ class Events extends Component{
   render() {
     //if (this.props.events == []) return (<div></div>);
 
-    let list = this.props.events.map((event) => {
+    let list = this.props.events.map((event,i) => {
       let detailLink = `/app/events/${event.id}`;
       let date = dateFormat(new Date(event.dt), "mmmm dS, yyyy");
-      return (
+      if ((this.props.params.id && (this.props.params.id == event.id)) || ( i === 0 && !this.props.params.id && !/new/.test(this.props.pathname))){
+
+         return (
+        <Link to={ detailLink } key={event.id} >
+        <li className="collection-item avatar teal white-text" key={event.id}>
+            <i className="material-icons circle teal">today</i>
+            <span className="title">{date}</span>
+        </li>
+        </Link>
+        );
+
+      }else{
+        return (
         <Link to={ detailLink } key={event.id} >
         <li className="collection-item avatar" key={event.id}>
             <i className="material-icons circle teal">today</i>
             <span className="title">{date}</span>
         </li>
         </Link>
-      )
-    })
+       );
+                      
+                    }
+                    
+
+                     },this)
+                  
 
     return (
     <div className="row">
@@ -36,12 +54,17 @@ class Events extends Component{
             <div className="col sm12 m5 lg5">
             <div className="choice-panel">
                   <ul className="collection">
-                    <li className="collection-item avatar">
+                    {/new/.test(this.props.pathname)?<li className="collection-item avatar teal white-text">
+                      <Link to="/app/events/new">
+                        <i className="material-icons circle teal white-text">today</i>
+                        <span className="title">Create New Event</span>
+                      </Link>
+                    </li>:<li className="collection-item avatar">
                       <Link to="/app/events/new">
                         <i className="material-icons circle teal">today</i>
                         <span className="title">Create New Event</span>
                       </Link>
-                    </li>
+                    </li>}
                     { list }
                   </ul>
             </div>
@@ -51,8 +74,8 @@ class Events extends Component{
               {this.props.children}
             </div> {/* <!--closes id main-panel--> */}
       </div>
-		);
-	}
+    );
+  }
 
 }
 
@@ -63,6 +86,7 @@ Events.propTypes = {
 function mapStateToProps(state, ownProps){
     return {
       events : state.events.all,
+      pathname: ownProps.location.pathname
     }
 }
 
