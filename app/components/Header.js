89b3e7967ca1664156608120d92c8928c751eb77
bhagya <bhagya@outlook.com>
2017-02-11 09:38:43 -0600

    header image added

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,8 +4,69 @@ import { Link } from 'react-router';
 
 class Header extends Component{
 
+
+constructor(props){
+
+  super(props);
+
+  this.state = {
+
+    member : this.props.member
+  }
+
+
+}
+
+
+componentWillReceiveProps(nextProps) {
+
+  var member;
+
+  if(  nextProps.members && nextProps.members.length){
+ 
+ console.log("here");
+ member =nextProps.members.find(e=> e.id === nextProps.member.id);
+
+}else{
+
+  member= nextProps.member;
+}
+
+
+this.setState({
+  member:member
+}); 
+
+}
   
+
 render(){
+
+var imglink;
+
+// if(  this.props.members && this.props.members.length){
+ 
+//  console.log("here");
+//  member =this.props.members.find(e=> e.id === this.props.member.id);
+
+// }else{
+
+//   member= this.props.member;
+// }
+
+
+
+
+  if(this.state.member.piclink){
+
+    imglink = "/img/"+  this.state.member.piclink ;
+  
+    }else{
+    
+    imglink = "/img/unknown.png";
+
+  }
+
 
 return (
     <div>
@@ -15,15 +76,21 @@ return (
             <li><a href="#">three</a></li>
         </ul>*/}
     <nav>
-        <div className="nav-wrapper">
+        <div className="navigation">
           <a href="#" className="brand-logo">Neighborhood Book Club</a>
-              <ul className="right hide-on-med-and-down">
-                  <li>Welcome, {this.props.member.fname}</li>
+              <ul className="right valign-wrapper">
+                  
                   {/*<li><a className="dropdown-button" href="#" data-activates="dropdown1">
                                     <i className="material-icons right">menu</i></a>
 
                   </li>*/}
-                  <li><a href="/bookclub.html"><button className="btn flat" >Sign out</button></a></li>
+
+                   <li id="profile-img" className="avatar white-text">
+                            <img  src={ imglink } alt="" className="circle valign" width="40px" height="40px"/>
+                           
+                  </li>
+                  <li> {this.state.member.fname}</li>
+                  <li><button className="btn flat" href="/bookclub.html">Sign out</button></li>
 
               </ul>
         </div>
@@ -36,14 +103,16 @@ return (
 
 Header.propTypes = {
 
-   member : React.PropTypes.object
+   member : React.PropTypes.object,
+   members: React.PropTypes.array
 
 }
 
 function mapStateToProps(state){
   return {
     
-    member: state.auth.profile
+    member: state.auth.profile,
+    members:state.members
 
   } 
 }
